36fb88bc4f0180ade3f2e445bbcc150472899d13
SEAKLAB <seak@cornell.edu>
2016-02-24 19:26:58 -0500

    Cityplot3d now supports input parsing!!! pass-throughs allow to pass info to called functions and now support multiple modes of input arguments

--- Cityplot_Dev/cityplot3d.m
+++ Cityplot_Dev/cityplot3d.m
@@ -1,50 +1,111 @@
-function [plotting,nMet,pltOpts,hdt]=cityplot3d(dist, metrics, archs, varargin)
+function [plotting,nMet,pltOpts,hdt]=cityplot3d(h,dist, metrics, archs, varargin)
 %cityplot3d Makes a 3d plot with bar graphs indicating the metrics at each
 %architecture and the position of the architecture minimizing the squared
 %error distance to the other points as given in dist matrix.
 %   dist--distance matrix. An n x n symmetric positive matrix of dstances
 %   between architecture i from architecture j.
 %   metrics--the scores to bar chart.
-figure
-hold on
 
-%dist=partMoveDistance(archs(1:30,:));
-%met=[results.sciences,results.costs];
+dummyFig=figure;
+%% set up for input parse
+p=inputParser;
+addOptional(p,'handle',dummyFig,@isgraphics)
+addRequired(p,'dist',@isnumeric)
+addRequired(p,'metrics',@isnumeric)
+addRequired(p,'archs')
 
-nMet=metrics-repmat(min(metrics,[],1),size(metrics,1),1); 
+% mdscale and cmdscale poke through
+addParameter(p,'UseClassic',true);
+addParameter(p,'MdscaleOptArgs',[]);
+% addParameter(p,'Criterion','stress');
+% addParameter(p,'Weights', []);
+% addParameter(p,'Start','cmdscale');
+% addParameter(p,'Replicates',1);
+% addParameter(p,'MdscaleOpt',[]);
+
+% skyscraper defaults and patch properties poke through
+addParameter(p,'BuildingHeight',[]);
+addParameter(p,'BuildingProp',[]);
+
+if(nargin<3)
+    error('not enough input arguments to cityplot3d');
+elseif(nargin==3)
+    parse(p,h,dist,metrics);
+else
+    parse(p,h,dist, metrics, archs,varargin{:});
+end
+
+%% normailization
+nMet=p.Results.metrics-repmat(min(p.Results.metrics,[],1),size(p.Results.metrics,1),1); 
 nMet=nMet./repmat(max(nMet,[],1),size(nMet,1),1);
 
-pltOpts=struct('skyscraperRegulation',[]);
+%% get the city locations with mdscale
+figure(p.Results.handle);
+hold on
+if(any(strcmp(p.UsingDefaults,'UseClassic')))
+    if(any(strcmp(p.UsingDefaults,'MdscaleOptArgs'))) %default to classic.
+        plotting=cmdscale(p.Results.dist,2);
+    else %default to mdscale if given arguments
+        plotting=mdscale(p.Results.dist,2,p.Results.MdscaleOptArgs{:});
+    end
+else
+    if(p.Results.UseClassic)
+        plotting=cmdscale(p.Results.dist,2); % use classic if directly called for
+    else
+        if(any(strcmp(p.UsingDefaults,'MdscaleOptArgs'))) %use mdscale with defaults if directly told not to use cmdscale.
+            plotting=mdscale(p.Results.dist,2);
+        else %pass through args if given.
+            plotting=mdscale(p.Results.dist,2,p.Results.MdscaleOptArgs{:});
+        end
+    end
+end
+% plotting=mdscale(p.Results.dist,2,'Criterion',p.Results.Criterion,'Weights',p.Results.Weights,'Start',p.Results.Start,'Replicates',p.Results.Replicates,'Options',p.Results.MdscaleOpt);
+% plotting=mdscale(p.Results.dist,2,'Criterion','sammon'); %seems to fix when wild variations in distance
+% plotting=mdscale(p.Results.dist,2); %Kruskal's Normalized Stress. "Classic"
+% plotting=cmdscale(p.Results.dist); plotting=plotting(:,1:min(2,size(plotting,2)));
 
-plotting=mdscale(dist,2,'Criterion','sammon'); %seems to fix when wild variations in distance
-% plotting=mdscale(dist,2); %Kruskal's Normalized Stress. "Classic"
-% plotting=cmdscale(dist); plotting=plotting(:,1:min(2,size(plotting,2)));
-colorEdgeByDist3d(dist,plotting,'auto');
-pltOpts.skyscraperRegulation=range(plotting(:,2))/10; %inspired by Ithaca's nonsense regulations we've elected by order of the mayor to name the data variable that limits the height of the bar plots/skyscrapers "skyscraperRegulation" such that housing is limited to 4 floor buildings.
-nodesWithBarGraph3d(plotting,nMet,pltOpts.skyscraperRegulation, varargin{:})
+%% Build Roads
+colorEdgeByDist3d(p.Results.dist,plotting,'auto');
+
+%% Build Skyscrapers
+if(any(strcmp(p.UsingDefaults,'BuildingHeight')))
+    BuildingHeight=range(plotting(:,2))/10;
+else
+    BuildingHeight=p.Results.BuildingHeight;
+end
+
+pltOpts.BuildingHeight=BuildingHeight;
+
+if(any(strcmp(p.UsingDefaults,'BuildingProp')))
+    nodesWithBarGraph3d(plotting,nMet,BuildingHeight);
+else
+    nodesWithBarGraph3d(plotting,nMet,BuildingHeight,p.Results.BuildingProp{:});
+end
+
+%% set default view
 campos([7.2964  -17.4457    8.8248]);
 % view([18,85]);
 
 %% standardize archLbls into a cell array of strings
-if(isnumeric(archs))
-    archLbls=cell(size(archs,1),1);
-    for(i=1:size(archs,1))
-        archLbls{i}=num2str(archs(i,:));
+if(isnumeric(p.Results.archs))
+    archLbls=cell(size(p.Results.archs,1),1);
+    for(i=1:size(p.Results.archs,1))
+        archLbls{i}=num2str(p.Results.archs(i,:));
     end
 else
-    if(iscell(archs))
-        archLbls=archs;
+    if(iscell(p.Results.archs))
+        archLbls=p.Results.archs;
     else
-        archLbls=cell(size(archs,1),1);
-        for(i=1:size(archs,1))
-            archLbls{i}=archs(i,:);
+        archLbls=cell(size(p.Results.archs,1),1);
+        for(i=1:size(p.Results.archs,1))
+            archLbls{i}=p.Results.archs(i,:);
         end
     end
 end
 
 %% create data cursor window
 % base_metLbls={'science','cost','programmatic risk','fairness'};
-% metLbls=mat2cell(base_metLbls(1:size(metrics,2)));
+% metLbls=mat2cell(base_metLbls(1:size(p.Results.metrics,2)));
 % aug_metLbls=cell(numel(metLbls)*2,1);
 % for(i=1:size(metLbls,2))
 %     aug_metLbls{i}=metLbls{i};
@@ -54,15 +115,15 @@ end
 % hdt = datacursormode;
 % set(hdt,'DisplayStyle','window');
 % set(hdt,'UpdateFcn',{@cityplotDataCursor,[plotting,zeros(size(plotting,1),1)],archLbls,...
-%     aug_metLbls,[metrics,nMet]});
-metLblsStr=num2str([1:size(metrics,2)]');
-metLbls=cell(size(metrics,2),1);
+%     aug_metLbls,[p.Results.metrics,nMet]});
+metLblsStr=num2str([1:size(p.Results.metrics,2)]');
+metLbls=cell(size(p.Results.metrics,2),1);
 for(i=1:size(metLbls,1))
     metLbls{i}=['objective', metLblsStr(i,:),': '];
 end
 hdt = datacursormode;
 set(hdt,'DisplayStyle','window');
-set(hdt,'UpdateFcn',{@cityplotDataCursor,[plotting,zeros(size(plotting,1),1)],archLbls,metLbls,metrics});
+set(hdt,'UpdateFcn',{@cityplotDataCursor,[plotting,zeros(size(plotting,1),1)],archLbls,metLbls,p.Results.metrics});
 
 hold off
 end
