53d0904cf44941b803e7e9259ff35b9ed23ded24
Nathan Knerr <importN@hotmail.com>
2016-09-21 19:15:19 -0400

    headersAndValuesToLabels can now take a set of nested cell 1-d arrays and forms a {{a,b},c} output for the contents.

--- headersAndValuesToLabels.m
+++ headersAndValuesToLabels.m
@@ -1,14 +1,28 @@
 function [labels]=headersAndValuesToLabels(header, values, varargin)
-p=inputParser;
-addRequired(p, 'header')
-addRequired(p, 'values')
-addParameter(p, 'headerValSeperator', ': ')
-addParameter(p, 'entrySeperator', ', ')
-parse(p, header, values, varargin{:})
+    p=inputParser;
+    addRequired(p, 'header')
+    addRequired(p, 'values')
+    addParameter(p, 'headerValSeperator', ': ')
+    addParameter(p, 'entrySeperator', ', ')
+    parse(p, header, values, varargin{:})
 
-labels=arrayfun(@(valRow) strjoin(...
-           arrayfun(@(i) [p.Results.header{i},p.Results.headerValSeperator,num2str(p.Results.values(valRow,i))],...
-                   1:size(p.Results.values,2), 'UniformOutput', false), p.Results.entrySeperator),...
-       1:size(values,1), 'UniformOutput', false);
+    if iscell(values)
+        labels=cellfun(@(cellVal) strjoin(...
+                   arrayfun(@(i) [p.Results.header{i},p.Results.headerValSeperator,cellArrayToDoubles(cellVal{i})],...
+                        1:length(p.Results.header),'UniformOutput',false), p.Results.entrySeperator),...
+               p.Results.values, 'UniformOutput',false);
+    else
+        labels=arrayfun(@(valRow) strjoin(...
+                   arrayfun(@(i) [p.Results.header{i},p.Results.headerValSeperator,num2str(p.Results.values(valRow,i))],...
+                           1:size(p.Results.values,2), 'UniformOutput', false), p.Results.entrySeperator),...
+               1:size(values,1), 'UniformOutput', false);
+    end
 return
     
+function [cellsStr]=cellArrayToDoubles(cellArr)
+    if iscell(cellArr)
+        cellsStr=['{ ', strjoin(cellfun(@cellArrayToDoubles,cellArr,'UniformOutput',false),'; '),' }'];
+    else
+        cellsStr=strjoin(arrayfun(@num2str,cellArr,'UniformOutput',false),', ');
+    end
+return
